9d048c06b11d1662de716548620a878808f16262
Chris Lemoine <chrismlemoine@gmail.com>
2026-06-28 14:35:33 -0500

    Configure robot drivetrain and localization
    Add mecanum drivetrain and pinpoint localization configuration constants. Includes motor names and directions, localization pod positions and encoder settings, and robot mass. Update the follower builder to use these new configurations.

--- Team00000/src/main/java/org/firstinspires/ftc/team00000/pedroPathing/Constants.java
+++ Team00000/src/main/java/org/firstinspires/ftc/team00000/pedroPathing/Constants.java
@@ -3,16 +3,45 @@ package org.firstinspires.ftc.team00000.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5.7); // kilograms
+
+    public static MecanumConstants driveConstraints = new MecanumConstants()
+            .maxPower(1.0)
+            .leftFrontMotorName("frontLeftDrive")
+            .rightFrontMotorName("frontRightDrive")
+            .leftRearMotorName("backLeftDrive")
+            .rightRearMotorName("backRightDrive")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-6.043)
+            .strafePodX(2.205)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .mecanumDrivetrain(driveConstraints)
                 .pathConstraints(pathConstraints)
                 .build();
     }
